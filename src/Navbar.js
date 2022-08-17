d3babd162fa15642e1f310efce8fdc55849168cb
rayhoward <raymond.a.howard@comcast.net>
2022-08-16 21:26:53 -0400

    *

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom"
 export default function Navbar() {
 
     return <nav className="nav">
-        <Link to="pages/" className="site-title">
+        <Link to="/pages/" className="site-title">
             Authentisite
         </Link>
         <ul>
