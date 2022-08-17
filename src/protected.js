009b170fb91a949bb40d6c7242c34c23970d2004
rayhoward <raymond.a.howard@comcast.net>
2022-08-16 20:40:38 -0400

    fixing authentication rerouting

--- src/protected.js
+++ src/protected.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 const Protected = ({ isLoggedIn, children }) => {
  if (!isLoggedIn) {
- return <Navigate to="/home"/>;
+ return <Navigate to="/"/>;
  }
  return children;
 };
